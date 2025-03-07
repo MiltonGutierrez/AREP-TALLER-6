21ebdc612fd7b59a4b071667b1c8e91560bfbe5b
MiltonGutierrez <gutierrezmilton31@gmail.com>
2025-03-07 10:28:14 -0500

    First Commit

---
+++ src/main/java/edu/escuelaing/arep/taller5/Taller5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.escuelaing.arep.taller5;
+
+/**
+ *
+ * @author milton.gutierrez-l
+ */
+public class Taller5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
