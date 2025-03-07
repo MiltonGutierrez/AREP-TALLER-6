b8df12e42c091083d7ca91a9daa3669acb321053
MiltonGutierrez <gutierrezmilton31@gmail.com>
2025-03-07 11:16:41 -0500

    keystore

--- src/main/java/edu/escuelaing/arep/taller5/Taller5.java
+++ src/main/java/edu/escuelaing/arep/taller5/Taller5.java
@@ -1,16 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package edu.escuelaing.arep.taller5;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
  * @author milton.gutierrez-l
  */
+@SpringBootApplication
 public class Taller5 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) { 
+        SpringApplication app = new SpringApplication(Taller5.class); 
+        app.setDefaultProperties(Collections.singletonMap("server.port", "5000")); 
+        app.run(args); 
     }
 }
