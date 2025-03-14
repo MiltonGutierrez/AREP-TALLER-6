bca8ca75ee2f04f86eccd34efbab6e3fb8c869c3
Milton Andres Gutierrez Lopez <80470554+MiltonGutierrez@users.noreply.github.com>
2025-03-13 19:05:32 -0500

    Final Back Changes

--- src/main/java/edu/escuelaing/arep/taller6/Taller6.java
+++ src/main/java/edu/escuelaing/arep/taller6/Taller6.java
@@ -13,15 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Taller6 {
     public static void main(String[] args) { 
         SpringApplication app = new SpringApplication(Taller6.class); 
-        app.setDefaultProperties(Collections.singletonMap("server.port", getPort())); 
+        app.setDefaultProperties(Collections.singletonMap("server.port", "443")); 
         app.run(args); 
-    }
-
-    static String getPort(){
-        String port = System.getenv("PORT");
-        if(port != null){
-            return port;
-        }
-        return "50001";
     }
 }
